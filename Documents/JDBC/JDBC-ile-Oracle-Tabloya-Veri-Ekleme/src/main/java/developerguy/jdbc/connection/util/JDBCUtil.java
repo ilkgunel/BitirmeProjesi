804f66a952be9ecf088e11c9525bc6b688306476
Batuhan APAYDIN <bapaydin67@gmail.com>
2016-01-21 23:47:39 +0200

    veri ekleme başarılı

---
+++ Documents/JDBC/JDBC-ile-Oracle-Tabloya-Veri-Ekleme/src/main/java/developerguy/jdbc/connection/util/JDBCUtil.java
@@ -0,0 +1,58 @@
+package developerguy.jdbc.connection.util;
+
+import java.sql.Connection;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.Date;
+
+/**
+ *  *
+ *  * @author Batuhan  
+ *
+ */
+public class JDBCUtil {
+
+    private static String DRIVER_CLASS = "oracle.jdbc.driver.OracleDriver";
+    private static String CONNECTION_URL = "jdbc:oracle:thin:@localhost:1521:xe";
+    private static String USER_NAME = "HR";
+    private static String PASSWORD = "12345";
+
+    public static Connection getConnection() {
+
+        Connection connection = null;
+
+        try {
+            Class.forName(DRIVER_CLASS);
+        } catch (ClassNotFoundException cnfe) {
+            System.out.println(cnfe.getMessage());
+        }
+        try {
+            connection = DriverManager.getConnection(CONNECTION_URL,
+                    USER_NAME, PASSWORD);
+            if (connection != null) {
+                return connection;
+            }
+        } catch (SQLException se) {
+            System.out.println(se.getMessage());
+        }
+        return connection;
+    }
+
+    public static void close(PreparedStatement ps, Connection con) {
+        try {
+            if (ps != null && con != null) {
+                ps.close();
+                con.close();
+            }
+        } catch (SQLException se) {
+            System.out.println(se.getLocalizedMessage());
+        }
+    }
+
+    public static java.sql.Date getSQLDate() {
+        java.util.Date date = new Date();
+        return new java.sql.Date(date.getTime());
+    }
+}
